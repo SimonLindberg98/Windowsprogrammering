a4a01662d613cd4e54aef402c7f35f57539f9f42
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-18 14:09:25 +0200

    Finished more exercises

--- uppg1-4/Form1.cs
+++ uppg1-4/Form1.cs
@@ -12,9 +12,26 @@ namespace uppg1_4
 {
     public partial class Form1 : Form
     {
+        int birthyear;
+        int year2000 = 2000;
+        int age;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(int.TryParse(textBox1.Text,out birthyear))
+            {
+                age = year2000 - birthyear;
+                label2.Text = "År 2000 fyllde du " + age.ToString() + " år";
+            }
+            else
+            {
+                label2.Text = "";
+            }
         }
     }
 }
