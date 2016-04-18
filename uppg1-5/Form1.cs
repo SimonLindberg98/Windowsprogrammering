c57c5d6d8e482e10e504849704d0dc4885419a2b
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-18 14:27:37 +0200

    Finished more exercises

---
+++ uppg1-5/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg1_5
+{
+    public partial class Form1 : Form
+    {
+        double value1 = 0;
+        double value2 = 0;
+        double result = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out value1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(textBox2.Text, out value2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            value1 = 0;
+            value2 = 0;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label1.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result = value1 + value2;
+            label1.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            result = value1 - value2;
+            label1.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            result = value1 * value2;
+            label1.Text = result.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            result = value1 / value2;
+            label1.Text = result.ToString();
+        }
+    }
+}
