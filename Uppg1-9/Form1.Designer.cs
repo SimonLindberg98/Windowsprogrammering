95756051b3681eebb949e99b02381dccd94eaf5b
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-21 13:21:19 +0200

    Finished another one
    A program which uses 5 dice to check how many attemps are needed to get
    5 of the same number.

--- Uppg1-9/Form1.Designer.cs
+++ Uppg1-9/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.kastaButton = new System.Windows.Forms.Button();
+            this.kastResultat = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.antalKastTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // kastaButton
+            // 
+            this.kastaButton.Location = new System.Drawing.Point(47, 51);
+            this.kastaButton.Name = "kastaButton";
+            this.kastaButton.Size = new System.Drawing.Size(105, 31);
+            this.kastaButton.TabIndex = 0;
+            this.kastaButton.Text = "Kasta";
+            this.kastaButton.UseVisualStyleBackColor = true;
+            this.kastaButton.Click += new System.EventHandler(this.kastaButton_Click);
+            // 
+            // kastResultat
+            // 
+            this.kastResultat.FormattingEnabled = true;
+            this.kastResultat.Location = new System.Drawing.Point(192, 51);
+            this.kastResultat.Name = "kastResultat";
+            this.kastResultat.Size = new System.Drawing.Size(120, 186);
+            this.kastResultat.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Antal kast:";
+            // 
+            // antalKastTextBox
+            // 
+            this.antalKastTextBox.Location = new System.Drawing.Point(192, 258);
+            this.antalKastTextBox.Name = "antalKastTextBox";
+            this.antalKastTextBox.Size = new System.Drawing.Size(120, 20);
+            this.antalKastTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 309);
+            this.Controls.Add(this.antalKastTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kastResultat);
+            this.Controls.Add(this.kastaButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button kastaButton;
+        private System.Windows.Forms.ListBox kastResultat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox antalKastTextBox;
     }
 }
 
--- Uppg1-9/Form1.cs
+++ Uppg1-9/Form1.cs
@@ -12,9 +12,30 @@ namespace Uppg1_9
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void kastaButton_Click(object sender, EventArgs e)
+        {
+
+            int t1, t2, t3, t4, t5;
+            do
+            {
+                t1 = rand.Next(1, 7);
+                t2 = rand.Next(1, 7);
+                t3 = rand.Next(1, 7);
+                t4 = rand.Next(1, 7);
+                t5 = rand.Next(1, 7);
+                counter++;
+                kastResultat.Items.Add("" +t1 + t2 + t3 + t4 + t5);
+            }
+            while (t1 != t2 || t1 != t3 || t1 != t4 || t1 != t5);
+            antalKastTextBox.Text = counter.ToString();
+
         }
     }
 }
