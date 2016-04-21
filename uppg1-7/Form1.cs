30fdbe6af620d5b9a2e527102cc1dee2ab56cb03
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-21 12:59:51 +0200

    Finished another one

---
+++ uppg1-7/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg1_7
+{
+    public partial class Form1 : Form
+    {
+        double height;
+        double weight;
+        double bmi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out height);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(textBox2.Text, out weight);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmi = (weight / (height * height));
+            label5.Text = bmi.ToString();
+            
+            if(bmi < 15)
+            {
+                label6.Text = "Mager";
+            }
+            else if(bmi > 15 && bmi < 20)
+            {
+                label6.Text = "Underviktig";
+            }
+            else if(bmi > 20 && bmi < 25)
+            {
+                label6.Text = "Normal";
+            }
+            else if(bmi > 25 && bmi < 30)
+            {
+                label6.Text = "Överviktig";
+            }
+            else if(bmi > 30 && bmi < 40)
+            {
+                label6.Text = "Fet";
+            }
+            else if(bmi > 40)
+            {
+                label6.Text = "Kraftigt fet";
+            }
+        }
+    }
+}
---
+++ uppg1-8/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg1_8
+{
+    public partial class Form1 : Form
+    {
+        int startValue;
+        int stopValue;
+        int incrementValue;
+        int sum;
+        double product = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out startValue);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(textBox2.Text, out stopValue);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(textBox3.Text, out incrementValue);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            product = 1;
+            listBox1.Items.Clear();
+            for (int i = startValue; i < stopValue +1; i += incrementValue)
+            {
+                listBox1.Items.Add(i);
+                textBox4.Text = (sum += i).ToString();
+                textBox5.Text = (product = product * i).ToString();
+                
+            }
+            
+
+        }
+    }
+}
