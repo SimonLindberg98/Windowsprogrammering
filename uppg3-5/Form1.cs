3406834f5b842faafc367b9d9042d068684d9d33
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-28 12:29:57 +0200

    Finished another one

---
+++ uppg3-5/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Text = "ASCII-tabell";
+            InitializeComponent();
+            for (int i = 32; i < 256; i++)
+            {
+                listBox1.Items.Add((char)i + "\t" + i);
+            }
+        }
+    }
+}
