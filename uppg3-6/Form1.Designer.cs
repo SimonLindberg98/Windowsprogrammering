5bc047df9dd5c834e1c4a41a465e8601981f30c1
SimonLindberg98 <simonlindberg98@gmail.com>
2016-05-01 16:03:45 +0200

    FInished another one

---
+++ uppg3-6/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace uppg3_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateBox = new System.Windows.Forms.TextBox();
+            this.monthButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datum (ååååmmdd)";
+            // 
+            // dateBox
+            // 
+            this.dateBox.Location = new System.Drawing.Point(50, 81);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.Size = new System.Drawing.Size(100, 20);
+            this.dateBox.TabIndex = 1;
+            this.dateBox.TextChanged += new System.EventHandler(this.dateBox_TextChanged);
+            // 
+            // monthButton
+            // 
+            this.monthButton.Location = new System.Drawing.Point(167, 78);
+            this.monthButton.Name = "monthButton";
+            this.monthButton.Size = new System.Drawing.Size(75, 23);
+            this.monthButton.TabIndex = 2;
+            this.monthButton.Text = "Visa";
+            this.monthButton.UseVisualStyleBackColor = true;
+            this.monthButton.Click += new System.EventHandler(this.monthButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 128);
+            this.Controls.Add(this.monthButton);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dateBox;
+        private System.Windows.Forms.Button monthButton;
+    }
+}
+
---
+++ uppg3-6/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg3_6
+{
+    public partial class Form1 : Form
+    {
+        int year, month, day, dayError = 0;
+        public Form1()
+        {
+            this.Text = "Månadsnamn";
+            InitializeComponent();
+        }
+
+        private void monthButton_Click(object sender, EventArgs e)
+        {
+            //Byggde in så att det inte får vara för många dagar i en viss månad
+            dayError = 0;
+
+            switch (month)
+            {
+                case 01:
+                    if(day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Januari");
+                    break;
+                case 02:
+                    if (day > 28)
+                    {
+                        //Räknar ut om det är skottår
+                        int.TryParse(dateBox.Text.Substring(0, 4), out year);
+                        if (dateBox.Text.Substring(2, 2) == "00")
+                        {
+                            if(year % 400 == 0)
+                            {
+                                if (day > 29)
+                                {
+                                    dayError = 1;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                dayError = 1;
+                                break;
+                            }
+                        }
+                        else if(year % 4 == 0)
+                        {
+                            if(day > 29)
+                            {
+                                dayError = 1;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            dayError = 1;
+                            break;
+                        }
+                        
+                    }
+                    MessageBox.Show("Februari");
+                    break;
+                case 03:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Mars");
+                    break;
+                case 04:
+                    if (day > 30)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("April");
+                    break;
+                case 05:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Maj");
+                    break;
+                case 06:
+                    if (day > 30)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Juni");
+                    break;
+                case 07:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Juli");
+                    break;
+                case 08:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Augusti");
+                    break;
+                case 09:
+                    if (day > 30)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("September");
+                    break;
+                case 10:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("Oktober");
+                    break;
+                case 11:
+                    if (day > 30)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("November");
+                    break;
+                case 12:
+                    if (day > 31)
+                    {
+                        dayError = 1;
+                        break;
+                    }
+                    MessageBox.Show("December");
+                    break;
+            }
+            if(dayError == 1)
+            {
+                MessageBox.Show("dd i datumet du skrev in är ogiltigt, försök igen");
+                dateBox.Text = dateBox.Text.Remove(6, 2);
+
+            }
+
+            
+        }
+
+        private void dateBox_TextChanged(object sender, EventArgs e)
+        {
+            if(dateBox.Text.Length > 8)
+            {
+                MessageBox.Show("Skriv in datumet med formatet ååååmmdd! Försök igen");
+                dateBox.Clear();
+            }
+            else if(dateBox.Text.Length == 8)
+            {
+                int.TryParse(dateBox.Text.Substring(4, 2), out month);
+                int.TryParse(dateBox.Text.Substring(6, 2), out day);
+                if (month < 1 || month > 12)
+                {
+                    MessageBox.Show("Månaden måste vara mellan 01 och 12");
+                }
+            }
+            
+        }
+    }
+}
