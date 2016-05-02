0c8400d4d84212bb43ddb6a328a36c3d1c0d3e48
SimonLindberg98 <simonlindberg98@gmail.com>
2016-05-02 12:53:56 +0200

    FInished another one

---
+++ uppg3-7/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace uppg3_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.formatButton = new System.Windows.Forms.Button();
+            this.formattedNumBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Telefonnummer";
+            // 
+            // numBox
+            // 
+            this.numBox.Location = new System.Drawing.Point(38, 48);
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(100, 20);
+            this.numBox.TabIndex = 1;
+            // 
+            // formatButton
+            // 
+            this.formatButton.Location = new System.Drawing.Point(144, 46);
+            this.formatButton.Name = "formatButton";
+            this.formatButton.Size = new System.Drawing.Size(75, 23);
+            this.formatButton.TabIndex = 2;
+            this.formatButton.Text = "Formatera";
+            this.formatButton.UseVisualStyleBackColor = true;
+            this.formatButton.Click += new System.EventHandler(this.formatButton_Click);
+            // 
+            // formattedNumBox
+            // 
+            this.formattedNumBox.Location = new System.Drawing.Point(225, 48);
+            this.formattedNumBox.Name = "formattedNumBox";
+            this.formattedNumBox.Size = new System.Drawing.Size(100, 20);
+            this.formattedNumBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Formaterat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 91);
+            this.Controls.Add(this.formattedNumBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.formatButton);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numBox;
+        private System.Windows.Forms.Button formatButton;
+        private System.Windows.Forms.TextBox formattedNumBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
