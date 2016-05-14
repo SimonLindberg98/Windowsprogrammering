4a4fc95c473105f039a615e66bb8a9a51bc388f2
SimonLindberg98 <simonlindberg98@gmail.com>
2016-05-14 11:51:34 +0200

    Finished some more

---
+++ uppg3-7/uppg3-7/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppg3_7
+{
+    public partial class Form1 : Form
+    {
+        string number;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void formButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
